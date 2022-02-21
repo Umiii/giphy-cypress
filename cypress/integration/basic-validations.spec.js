6572df66fc170064fd7aa4f94924b1ca24bc93ac
Umi <umy.ikem@gmail.com>
2022-02-21 01:47:35 +0100

    completed upload test

--- cypress/integration/basic-validations.spec.js
+++ cypress/integration/basic-validations.spec.js
@@ -4,6 +4,7 @@ describe('Basic Giphy Validations', () => {
 
     before(() => {
         cy.visit(Cypress.env('baseUrl'))
+        
     })
 
 
@@ -19,9 +20,9 @@ describe('Basic Giphy Validations', () => {
         const search_query = 'black'
 
         //type a search query and type Enter key
-        cy.get(selectors.home.searchInput).type(search_query).type("{enter}")
-        cy.get(selectors.home.searchQueryHeading).should('include.text', search_query)
-        cy.get(selectors.home.headingCount).then(($count) => {
+        cy.get(selectors.homePage.searchInput).type(search_query).type("{enter}")
+        cy.get(selectors.homePage.searchQueryHeading).should('include.text', search_query)
+        cy.get(selectors.homePage.headingCount).then(($count) => {
             let count = $count.attr("data-gif-count")
             // Remove comma
             count = parseFloat(count.replace(/,/g,''))
@@ -29,11 +30,25 @@ describe('Basic Giphy Validations', () => {
             expect(count).to.be.at.least(1)
             
         })
-        cy.get(selectors.home.listGif).should('be.visible')
+        cy.get(selectors.homePage.listGif).should('be.visible')
     })
     
     it("should upload a file successfully", () => {
-        cy.get(selectors.home.uploadBtn)
+       
+        cy.get(selectors.homePage.username).click()
+        cy.get(selectors.homePage.email).type("umy.ikem@gmail.com")
+        cy.get(selectors.homePage.password).type("born1983$$")
+        cy.contains('Log in').click()
+        cy.get(selectors.homePage.uploadBtn).click()
+        cy.get(selectors.uploadPage.inputFile).selectFile('cypress\\fixtures\\rand.gif')
+        cy.get(selectors.uploadPage.downloadBtn).eq(0).click().then(($download_btn) => {
+            cy.location('pathname').should('include', 'stickers/transparent')
+            cy.get(selectors.uploadPage.uploadSuccessMessage).should('exist')
+        })
+       
+        // attachFile({filePath: "rand.gif", encoding: 'utf-8'})  
+                  
+
     })
 
 })
--- cypress/support/resources/selectors.js
+++ cypress/support/resources/selectors.js
@@ -1,9 +1,20 @@
 export const selectors = {
-    "home" : {
-        "searchInput" : ".Input-sc-w75cdz",
-        "searchQueryHeading" : ".Title-sc-1nnws5s",
-        "headingCount"  : ".Title-sc-1nnws5s > span",
-        "listGif" : ".giphy-gif",
-        "uploadBtn" : "_2bR0VNOr0505p4eSudMi0k"
+    "homePage" : {
+        "searchInput" : `.Input-sc-w75cdz`,
+        "searchQueryHeading" : `.Title-sc-1nnws5s`,
+        "headingCount"  : `.Title-sc-1nnws5s > span`,
+        "listGif" : `.giphy-gif`,
+        "uploadBtn" : `._2bR0VNOr0505p4eSudMi0k`,
+        "uploadInput" : `.DropZone-sc-1871h8g > input`,
+        "loginBtn"  : `Username-sc-1dgyzmk`,
+        "username"  : `.Username-sc-1dgyzmk`,
+        "email" :   `input[type='email']`,
+        "password"  :   `input[type='password']`,
+        
+    },
+    "uploadPage" : {
+        "inputFile" : `input[type="file"]:first`,
+        "downloadBtn" : `.Wrapper-sc-1iv14re > .GradientBlock-sc-agmctt`,
+        "uploadSuccessMessage" : `.flash-message__Text-grey4k-2`
     }
 }
